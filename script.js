e09bb011b52197b967dd566a90e8b8661a6abec4
Gabbie sayuri <anagabriellarw@gmail.com>
2026-03-11 01:09:04 -0300

    Add files via upload
    first version of my portfolio site

---
+++ script.js
@@ -0,0 +1,12 @@
+let botao = document.getElementById("botao");
+
+botao.addEventListener("click", function(){
+    alert("Button working!");
+
+    .card img{
+width:100%;
+border-radius:10px;
+margin-bottom:10px;
+}
+
+});
